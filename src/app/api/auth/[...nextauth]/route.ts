408c2bb89ad26fd584b0879810ea06fce05d69f8
axantillon <aantillon.leiva@gmail.com>
2023-10-06 14:44:44 -0600

    add init auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,21 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-const handler = NextAuth({
+const handler: AuthOptions = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!!,
       clientSecret: process.env.GOOGLE_SECRET!!,
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      
+      return true;
+    },
+  },
 });
 
 export { handler as GET, handler as POST }
--- src/app/hunt/page.tsx
+++ src/app/hunt/page.tsx
@@ -1,10 +1,13 @@
+'use client'
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
 
 
 export default function Hunt() {
 
     return (
-        <div className="">
-            
+        <div className="w-full h-full flex items-center justify-center py-28">
+            <Button onClick={() => {signIn("google")}}>Log In</Button>
         </div>
     )
 }
