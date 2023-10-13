bbdf30f044e093d327a341eaf121351c1bd22d60
aguilarcarboni <aguilarcarboni@gmail.com>
2023-10-13 11:12:16 -0600

    Update sortTeams.ts

--- src/lib/utils/sortTeams.ts
+++ src/lib/utils/sortTeams.ts
@@ -5,7 +5,7 @@ import { SortedTeams } from "@/components/Scoreboard";
 export const sortTeams = (teams: User[]) => {
 
     const teamsScore = teams.map((team) => calculateScore(team))
-    const sortedTeams = teamsScore.sort((a,b) => b.score - a.score)
+    const sortedTeams = teamsScore.sort((a,b) => b.score - a.score) // add sort by date
 
     return sortedTeams
 }
